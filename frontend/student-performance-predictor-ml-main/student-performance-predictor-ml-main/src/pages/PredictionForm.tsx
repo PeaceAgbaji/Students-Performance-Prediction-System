7cb7dd42940f2f1a59a13366995f5011ed09cf73
PeaceAgbaji <peaceagbaji8@gmail.com>
2025-12-01 03:08:48 +0100

    Update API URL to live Render endpoint

--- frontend/student-performance-predictor-ml-main/student-performance-predictor-ml-main/src/pages/PredictionForm.tsx
+++ frontend/student-performance-predictor-ml-main/student-performance-predictor-ml-main/src/pages/PredictionForm.tsx
@@ -78,7 +78,7 @@ const PredictionForm = () => {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://students-performance-prediction-system-production.up.railway.app/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
